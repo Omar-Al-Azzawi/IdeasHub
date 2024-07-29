69fd2b96058c868987d9142622809276cba99dfc
Omar Alazzawi <alazzawiomar01@gmail.com>
2024-07-29 20:13:40 +0300

    update PW github action

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -9,10 +9,8 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'https://my-ideas-hub.vercel.app/',
     ignoreHTTPSErrors: true,
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
 
@@ -22,11 +20,4 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
--- e2e/tests/home.spec.ts
+++ e2e/tests/home.spec.ts
@@ -2,6 +2,5 @@ import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   await page.goto('/');
-  await page.waitForTimeout(5000)
   await expect(page).toHaveTitle(/ideahub/i);
 });
